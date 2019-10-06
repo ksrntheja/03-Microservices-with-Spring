d119ebd96c2395e28c2c1de42a5e5523ac9dfbed
ksrntheja <srntkolla@gmail.com>
2019-10-06 19:08:28 +0530

    06-Making microservices calls without RestTemplate.

--- movie-catalog-service/src/main/java/io/ksrntheja/movie/catalog/service/models/CatalogItem.java
+++ movie-catalog-service/src/main/java/io/ksrntheja/movie/catalog/service/models/CatalogItem.java
@@ -35,5 +35,5 @@ public class CatalogItem {
     public void setRating(int rating) {
         this.rating = rating;
     }
-    
+
 }
--- movie-catalog-service/src/main/java/io/ksrntheja/movie/catalog/service/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/ksrntheja/movie/catalog/service/resources/MovieCatalogResource.java
@@ -1,12 +1,16 @@
 package io.ksrntheja.movie.catalog.service.resources;
 
 import io.ksrntheja.movie.catalog.service.models.CatalogItem;
+import io.ksrntheja.movie.catalog.service.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -16,9 +20,24 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
         System.out.println("User Id :: " + userId);
 
-        return Collections.singletonList(
-                new CatalogItem("Movie1", "Movie1Desc", 5)
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
         );
+
+/*        List<CatalogItem> list = new ArrayList<>();
+        for (Rating rating : ratings) {
+            CatalogItem catalogItem = new CatalogItem("Movie1", "Movie1Desc", 5);
+            list.add(catalogItem);
+        }
+        return list;*/
+
+        return ratings.stream().map(rating -> new CatalogItem("Movie1", "Movie1Desc", 5))
+                .collect(Collectors.toList());
+
+/*        return Collections.singletonList(
+                new CatalogItem("Movie1", "Movie1Desc", 5)
+        );*/
     }
 
 }
