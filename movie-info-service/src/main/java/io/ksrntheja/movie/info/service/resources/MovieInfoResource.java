150d34fd1e388511725fb47943d961adac05058f
ksrntheja <srntkolla@gmail.com>
2019-10-06 12:58:51 +0530

    03-Built movie-info-service.

---
+++ movie-info-service/src/main/java/io/ksrntheja/movie/info/service/resources/MovieInfoResource.java
@@ -0,0 +1,17 @@
+package io.ksrntheja.movie.info.service.resources;
+
+import io.ksrntheja.movie.info.service.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieInfoResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable String movieId) {
+        return new Movie(movieId, "Movie1Name");
+    }
+
+}
